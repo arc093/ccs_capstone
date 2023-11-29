7d9da2507fd8ae7bcea97c030d018006f47d4e07
arc093 <152421805+arc093@users.noreply.github.com>
2023-11-29 11:43:30 -0800

    changed comments

--- arduino/prototype1/prototype1.ino
+++ arduino/prototype1/prototype1.ino
@@ -1,25 +1,12 @@
 /*
-  Button
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
+  The circuit
   - LED attached from pin 13 to ground through 220 ohm resistor
   - pushbutton attached to pin 2 from +5V
   - 10K resistor attached to pin 2 from ground
+  - Potentiometer attached to pin 3, pin 4
 
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
 
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
 */
 
 // constants won't change. They're used here to set pin numbers:
