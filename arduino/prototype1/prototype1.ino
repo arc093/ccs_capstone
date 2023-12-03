d3fef966cd58d980b377478ee47a03ddaae6f131
mabelespinozachavez <152745485+mabelespinozachavez@users.noreply.github.com>
2023-12-02 16:07:07 -0800

    Changed pin numbers and main loop

--- arduino/prototype1/prototype1.ino
+++ arduino/prototype1/prototype1.ino
@@ -10,10 +10,10 @@
 */
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-const int durPotPin = 3;
-const int speedPotPin = 4;
+const int buttonPin = 2;        // the number of the pushbutton pin
+const int ledPin =  6;          // the number of the LED pin
+const int durPotPin = A5;       //orange wire
+const int speedPotPin = A0;     //red wire
 
 
 
@@ -42,21 +42,16 @@ void loop() {
   durationPar = map(analogRead(durPotPin), 0, 1023, 0.1, 5);
   speedPar = map(analogRead(speedPotPin), 0, 1023, 1, 5);
 
-  curTime = millis() - startTime;
-  //if the timer is done, turn off the led, if its still going, keep the led on
-  if (curTime > durationPar) {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  } else {
-    // turn LED on with brightness according to speed:
-    analogWrite(ledPin, speedPar);    
-  }
-  
-
   // check if the user is holding the button, continuously start a new timer so when they stop holding the button the timer is released and starts counting
   if (buttonState == HIGH) {
       // start a new timer
       startTime = millis();
+      analogWrite(ledPin,speedPar);
+      curTime = millis() - startTime;
+      // if the timer is done, turn off the LED, if its still going, keep the LED on
+      while (curTime < durationPar) {
+        // turn LED on with brightness according to speed:
+        analogWrite(ledPin,speedPar);
+      }
   } 
-
 }
